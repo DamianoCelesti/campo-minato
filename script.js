5287b487e6d4aa7a3bb943dc8fdc4d7e87ec0369
Damiano Celesti <Damio9422@hotmail.it>
2025-09-18 15:31:11 +0200

    add event listener + css

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const contenitore = document.querySelector(".contenitore");
+const scoreId = document.getElementById("score")
 
 
 const NUM_CELLE = 49;
 const NUM_BOMBE = 16;
 let punteggio = 0;
 let gameOver = false;
+
+function aggiornaPunteggio() {
+    scoreId.textContent = "Punteggio: " + punteggio;
+}
+
+aggiornaPunteggio();
+
+
 
 const celle = [];
 
@@ -26,8 +35,14 @@ for (let i = 0; bombe.length < NUM_BOMBE; i++) {
     }
 
 }
-console.log(bombe);
-console.log(celle);
+console.log("bombe", bombe);
+console.log("celle", celle);
+
+
+for (let k = 0; k < bombe.length; k++) {
+    let indiceBomba = bombe[k]
+    celle[indiceBomba].bomba = true
+}
 
 
 for (let i = 0; i < NUM_CELLE; i++) {
@@ -35,9 +50,42 @@ for (let i = 0; i < NUM_CELLE; i++) {
     const nuovoDiv = document.createElement("div")
     nuovoDiv.classList.add("cella");
     nuovoDiv.setAttribute("id", i);
+
+    nuovoDiv.addEventListener("click", () => {
+        if (gameOver)
+            return;
+
+        const cella = celle[i];
+        if (cella.bomba) {
+            alert("Hai perso!");
+            gameOver = true;
+
+            for (let j = 0; j < celle.length; j++) {
+                if (celle[j].bomba) {
+                    document.getElementById(j).classList.add("bomba");
+                }
+            }
+
+            return;
+        }
+
+        if (!cella.scoperta) {
+            cella.scoperta = true;
+            nuovoDiv.classList.add("scoperta");
+            punteggio++;
+            aggiornaPunteggio();
+        }
+
+        if (punteggio === NUM_CELLE - NUM_BOMBE) {
+            alert("Hai vinto!")
+            gameOver = true
+        }
+    });
+
+
     contenitore.appendChild(nuovoDiv);
 }
 
 
-console.log(celle.length);
-console.log(celle[0]);     
+//console.log(celle.length);
+//console.log(celle[0]);     
